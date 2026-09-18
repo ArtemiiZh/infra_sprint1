ff973454721ae3117ef85661281b3e852c95a13a
Artemii <artemii@example.com>
2026-09-18 11:19:29 +0000

    Исправление: настроен доступ ALLOWED_HOSTS, отключен DEBUG и обновлен файл доступов

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,15 @@
 import os
+
+
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-kitty-super-secret-key-2026')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.250.32.249', 'kittygram.artemii-project.ddns.net', 'taski.artemii-project.ddns.net', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
